14c6dd02a2d4790bf222a7fd3e61c36282581399
Brian-Samson01 <brian.s.mwangi01@gmail.com>
2025-11-10 21:27:35 +0300

    button component

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,10 +1,18 @@
 import Card from "@/components/Card";
+import Button from "@/components/Button";
+
 
 const Landing: React.FC = () => {
   return (
     <div>
       <h1 className="text-xl font-extralight">Landing Page</h1>
       <Card />
+      <div className="flex gap-4 mt-4">
+        <Button title="Small Rounded" styles="text-sm rounded-sm" />
+        <Button title="Medium Rounded" styles="text-base rounded-md" />
+        <Button title="Large Rounded" styles="text-lg rounded-full" />
+      </div>
+
     </div>
   );
 };
